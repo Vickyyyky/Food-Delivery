7158cd5bfd494b8a92fcadf0e96b76f6461e193d
Vicky Kumar <vk2388275@gmail.com>
2025-05-23 02:58:48 +0530

    Initial commit

--- Backend/app.js
+++ Backend/app.js
@@ -4,6 +4,7 @@ const cookieParser=require("cookie-parser");
 const userRoutes=require("./src/routers/user.routes");
 const foodRoutes=require("./src/routers/food.routes");
 const cartRoutes=require("./src/routers/cart.routes")
+const orderRoutes = require("./src/routers/order.routes");
 const error = require("./src/middlewares/error.middleware");
 const app=express();
 //!middleware Section
@@ -13,6 +14,7 @@ app.use(cookieParser());
 app.use("/users/v1",userRoutes);
 app.use("/foods/v1",foodRoutes);
 app.use("/carts/v1",cartRoutes);
+app.use("/orders/v1", orderRoutes);
 
 
 
